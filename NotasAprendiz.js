4d187565acd225b5767cc9e80d3e954ee1a7c17a
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-02-08 15:25:02 -0500

    Creacion de archivos js,html,css con los datos iniciales. Creacion de titulos,labels,inpust,uniones de estilos y script. Creacion de constantes con su respectivo llamado. DMV

---
+++ NotasAprendiz.js
@@ -0,0 +1,5 @@
+//Guardamos en constantes las notas que ingrese el usuario
+const primerNotaAprendiz = queryElementById('#primerNota');
+const segundaNotaAprendiz = queryElementById('#segundaNota');
+const tercerNotaAprendiz = queryElementById('#tercerNota');
+
