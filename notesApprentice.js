dbe303fb281dd8af439e0d89f737ccaae3fcfee4
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-02-10 12:55:06 -0500

    add: ''Cambios de idioma de español a ingles, tambien correcciones en el codigo"

---
+++ notesApprentice.js
@@ -0,0 +1,5 @@
+//Guardamos en constantes las Notes que ingrese el usuario
+const firstNoteApprentice = queryElementById('#firstNote');
+const secondNoteApprentice = queryElementById('#secondNote');
+const thirdNoteApprentice = queryElementById('#thirdNote');
+
